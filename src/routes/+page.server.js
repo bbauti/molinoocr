7ad511cde3da905bec0ac9f84e2d2750217ac8ae
bbauti <bautistaigarzabal11@gmail.com>
2023-06-29 07:52:13 -0300

    init

---
+++ src/routes/+page.server.js
@@ -0,0 +1,45 @@
+/** @type {import('./$types').Actions} */
+import { writeFile } from 'fs/promises';
+// import { fs } from 'fs'
+import { createWorker } from 'tesseract.js';
+import { convert } from 'pdf-img-convert';
+// import { exportImages } from 'pdf-export-images'
+import { json } from '@sveltejs/kit';
+
+
+
+async function logTesseract(file) {
+    const worker = await createWorker({
+        logger: (m) => console.log(m)
+    });
+
+    (async () => {
+        await worker.loadLanguage('spa');
+        await worker.initialize('spa');
+        const {
+            data: { text }
+        } = await worker.recognize(file);
+        await worker.terminate();
+        console.log(text)
+        return text
+    })();
+}
+
+export const actions = {
+	submit: async ({ request }) => {
+        const data = await request.formData()
+        for (const [key, value] of data.entries()) {
+            let file = Buffer.from(await value.arrayBuffer(), 'base64')
+            await writeFile(`./pdf/${value.name}`, await file)
+            let image = convert(`./pdf/${value.name}`,{
+                width: 2000,
+                height: 2000,
+                page_numbers: [1],
+            })
+            await writeFile(`./images/${value.name}.png`, await image)
+            let text = await logTesseract(`./images/${value.name}.png`)
+            return text;
+          }
+        return { file: image };
+    },
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { enhance } from '$app/forms';
+	export let form;
+	if (form?.text === 'hey') {
+	}
+</script>
+
+<form method="POST" action="?/submit" use:enhance enctype="multipart/form-data">
+	<input name="file" type="file" accept="application/pdf" multiple />
+	<button>Upload</button>
+</form>
